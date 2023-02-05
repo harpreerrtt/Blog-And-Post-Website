954459ee0255bd77d0936982a771bf9a8762236d
harpreerrtt <gobindsinghharpreet@gmail.com>
2023-02-04 20:47:27 -0500

    part_1_commit

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-//hifgfrtgfbgbdfgit khjbb
+var express = require("express");
+var app = express();
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+// call this function after the http server starts listening for requests
+function onHttpStart() {
+    console.log("Express http server listening on " + HTTP_PORT);
+}
+
+//redirect / to /about function 
+app.get("/", (req, res) => {
+    res.redirect('/about') //redirection
+})
+
+//buil in static middleware
+app.use(express.static('public'));
+
+//ouputing about.html to /about
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + '/views/about.html')
+})
+
+
+
+
+
+
+
+//listen to 8080 port 
+app.listen(HTTP_PORT, onHttpStart); //call onHttpStart
